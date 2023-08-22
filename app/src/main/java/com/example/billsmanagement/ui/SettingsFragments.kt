7b12aaf0a1efc843d6c1562cb76e63c06feb3c06
Annetjepkorirr <you@example.com>
2023-08-22 19:23:09 +0300

    Added Fragments

---
+++ app/src/main/java/com/example/billsmanagement/ui/SettingsFragments.kt
@@ -0,0 +1,20 @@
+package com.example.billsmanagement.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.billsmanagement.R
+
+
+class SettingsFragments : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_settings_fragments, container, false)
+    }
+
+
+}
